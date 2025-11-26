a967786de30fc35e7f709317b1d91738c6037be3
danieljart <danieljart@gmail.com>
2025-11-26 14:46:27 -0300

    Fix scroll position on route change

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import ServicosPage from "./pages/ServicosPage";
 import FachadasPage from "./pages/FachadasPage";
@@ -22,6 +23,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/servicos" element={<ServicosPage />} />
